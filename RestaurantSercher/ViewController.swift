d5b49a8a7131570643e05b0e6c63b3826d103f64
ninjin <fumiya8811@gmail.com>
2019-04-27 14:52:05 +0900

    Initial Commit

---
+++ RestaurantSercher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestaurantSercher
+//
+//  Created by 吉野史也 on 2019/04/27.
+//  Copyright © 2019 yoshinofumiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
