71d4c119fb4c90e02adac278d4eb75f1d8e19976
ninjin <fumiya8811@gmail.com>
2019-04-28 18:03:19 +0900

    created StartView, StartViewController

--- RestaurantSercher/AppDelegate.swift
+++ RestaurantSercher/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  RestaurantSercher
-//
-//  Created by 吉野史也 on 2019/04/27.
-//  Copyright © 2019 yoshinofumiya. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
---
+++ RestaurantSercher/Controller/StartViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class StartViewController: UIViewController {
+
+    @IBOutlet weak private var rangeLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeRangeButtonPressed(_ sender: UIButton) {
+    }
+
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+    }
+}
