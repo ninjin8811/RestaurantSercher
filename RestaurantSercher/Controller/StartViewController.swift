dd2142b429c103148b29dddcad5dc2bca3844c3e
ninjin <fumiya8811@gmail.com>
2019-04-30 01:01:29 +0900

    Added the key LocationUsage to info.plist Implemented getting current location method

--- RestaurantSercher/Controller/StartViewController.swift
+++ RestaurantSercher/Controller/StartViewController.swift
@@ -1,3 +1,4 @@
+import CoreLocation
 import UIKit
 
 class StartViewController: UIViewController {
@@ -5,15 +6,29 @@ class StartViewController: UIViewController {
     @IBOutlet weak private var pickerKeyboardButton: PickerViewKeyboard!
     @IBOutlet weak private var rangeLabel: UILabel!
 
+    var locationManager = CLLocationManager()
+
     let pickerDataSource: [String] = ["100m", "300m", "500m", "800m", "1000m", "1500m", "2000m", "3000m"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         pickerKeyboardButton.delegate = self
+        locationManager.delegate = self
+
+        setupLocationManager()
     }
 
     @IBAction func searchButtonPressed(_ sender: UIButton) {
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            locationManager.startUpdatingLocation()
+            //ここに位置情報を表示する処理を書く
+            print("リクエスト")
+            locationManager.requestLocation()
+//            performSegue(withIdentifier: "goToSearchView", sender: self)
+        } else {
+            print("位置情報の取得を許可されていません")
+        }
     }
 }
 
@@ -34,5 +49,35 @@ extension StartViewController: PickerViewKeyboardDelegate {
     func didDone(sender: PickerViewKeyboard, selectedData: String) {
         rangeLabel.text = "半径 \(selectedData) 以内"
         sender.resignFirstResponder()
+    }
+}
+
+extension StartViewController: CLLocationManagerDelegate {
+    func setupLocationManager() {
+        locationManager.requestWhenInUseAuthorization()
+
+        let status = CLLocationManager.authorizationStatus()
+        if status == .authorizedWhenInUse {
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.distanceFilter = 1000
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+
+            let latitude = String(location.coordinate.latitude)
+            let longitude = String(location.coordinate.longitude)
+
+            let params: [String: String] = ["lat": latitude, "lon": longitude]
+
+            print(params)
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("位置情報の取得に失敗しました")
     }
 }
